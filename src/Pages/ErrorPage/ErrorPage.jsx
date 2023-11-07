da194f2cd0752c1c7666053cd4e3c59c82ebf5ce
Md Nirab <nabilnirab255@gmail.com>
2023-11-07 12:28:04 +0600

    react-rating install

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
@@ -9,24 +10,22 @@ const ErrorPage = () => {
                     <p className="mt-4 text-gray-500 dark:text-gray-400">Sorry, the page you are looking for doesn`&apos;`t exist.Here are some helpful links:</p>
 
                     <div className="flex items-center mt-6 gap-x-3">
-                        <button className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 rtl:rotate-180">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
-                            </svg>
+                        <Link to={'/'}>
+                            <button className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 rtl:rotate-180">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                                </svg>
 
 
-                            <span>Go back</span>
-                        </button>
-
-                        <button className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
-                            Take me home
-                        </button>
+                                <span>Go back</span>
+                            </button> 
+                        </Link>
                     </div>
                 </div>
 
-                {/* <div className="relative w-full mt-12 lg:w-1/2 lg:mt-0">
-                    <img className="w-full max-w-lg lg:mx-auto" src="https://merakiui.com/images/components/illustration.svg" alt="">
-                </div> */}
+                <div className="relative w-full mt-12 lg:w-1/2 lg:mt-0">
+                    <img className="w-full max-w-lg lg:mx-auto" src="https://merakiui.com/images/components/illustration.svg" alt="" />
+                </div>
             </div>
         </section>
     );
--- src/Pages/Home/Offer.jsx
+++ src/Pages/Home/Offer.jsx
@@ -10,7 +10,7 @@ const Offer = () => {
                             <p className="mb-5 text-6xl text-white font-bold">$99/-</p>
                         </div>
                         <div className="">
-                            <div className="bg-[#f4364fe7] hover:bg-[#d8c42a] w-72 h-72 text-center border-4 border-white rounded-full">
+                            <div className="bg-[#f4364fe7] hover:bg-[#2a64d8] w-72 h-72 text-center border-4 border-white rounded-full">
                                 <h2 className="text-white text-4xl font-bold mt-12 mb-4">Get <p className="text-6xl">70%</p> Off</h2>
                                 <span className="text-white border p-2 rounded-full ">Use code: RG5481WERQ</span>
                             </div>
